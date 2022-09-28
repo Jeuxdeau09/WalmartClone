efa179affeeb961da9f7d521b63fabb994d3fe03
unknown <jeuxdeau2009@gmail.com>
2022-09-28 10:20:03 -0400

    Project Setup with images

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View style={{alignItems: 'center'}}>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+export default App;
